567c29984891b6023e4d1d21837ff1db5b1f7959
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2016-10-09 20:22:42 -0400

    Clean up retweeting module

--- retweet.py
+++ retweet.py
@@ -14,47 +14,35 @@ from pushbullet import Pushbullet
 # initialize api and authenticate via o-auth. returns tweepy API() object
 def tweepy_setup():
     # keys imported from secrets.py
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
     auth.secure = True
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(secrets.access_token, secrets.access_token_secret)
 
     return tweepy.API(auth)
 
 
 # retweet any toast-related tweets from people I follow
-def retweet_statuses():
-    dir = os.path.abspath(os.path.dirname(sys.argv[0]))
-
+def retweet_statuses(pattern):
     timeline = tweepy_api.home_timeline(count=1000)
 
     print "Ran at " + time.strftime("%H:%M:%S")
     print "Retrieved %d statuses\n" % (len(timeline),)
 
-    if len(timeline) == 0:
-        sys.exit()
-
-    with open(dir + "/regex.p", "rb") as f:
-        pattern = pickle.load(f)  # compiled regex object for matching toast related content
-
     for status in timeline:
         text = status.text.lower()
-        if re.search(pattern, text) and not status.retweeted and status.user.id != tweepy_api.me().id:
+        if re.search(pattern, text) and not status.retweeted and not status.user.protected and status.user.id != tweepy_api.me().id:
             tweepy_api.retweet(status.id)
             # push notification to phone about retweets
             phone.push_note("Retweeted " + status.user.name + ":", text)
 
+
 if __name__ == "__main__":
     # setup pushbullet to send push notifications about tweets to my phone
     # pb_phone defined in secrets.py
-    pb = Pushbullet(pb_key)
-    phone = filter(lambda x: x.nickname == pb_phone, pb.devices)[0]
+    pb = Pushbullet(secrets.pb_key)
+    phone = filter(lambda x: x.nickname == secrets.pb_phone, pb.devices)[0]
+
+    pattern = re.compile(u"(\U0001F35E)|(bread)|(toast)", re.UNICODE)
 
     tweepy_api = tweepy_setup()
-    
-    retweet_statuses()
-
-    with open(toaster.make_toast(), "rb") as toast:
-        tweepy_api.update_with_media("toast.png", "it's toast time!", file=toast)
-
-        file_data = pb.upload_file(toast, "toast.png")
-        phone.push_file(**file_data)
+    retweet_statuses(pattern)
